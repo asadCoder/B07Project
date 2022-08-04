3a49d1470bbfdd326fa2b7fe5c011d2ed998acb7
Vikram Narra <vchandra.narra@gmail.com>
2022-08-03 20:40:52 -0400

    Navigation Bar

--- app/src/main/java/com/example/b07project/MainActivity.java
+++ app/src/main/java/com/example/b07project/MainActivity.java
@@ -1,31 +1,49 @@
 package com.example.b07project;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    MyEventsFragment myEvents = new MyEventsFragment();
+    ViewVenuesFragment settingsFragment = new ViewVenuesFragment();
+    UpcomingFragment notificationFragment = new UpcomingFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        val bottomNavigationView = findViewById< BottomNavigationView>(R.id.bottomNavigationView)
-        val
+        bottomNavigationView  = findViewById(R.id.bottom_navigation);
 
-    }
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, myEvents).commit();
 
-    public void logout(View view){
-        FirebaseAuth.getInstance().signOut();
-        startActivity(new Intent(getApplicationContext(),Login.class));
-        finish();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.venues:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, myEvents).commit();
+                        return true;
+                    case R.id.upcoming:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,notificationFragment).commit();
+                        return true;
+                    case R.id.myevents:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,settingsFragment).commit();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+
     }
 }
